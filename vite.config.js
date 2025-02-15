e8d119a260ef343eca0b3df47970cba2e124c901
ckj <2313364746@qq.com>
2025-02-15 15:12:48 +0800

    blob init

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import Pages from 'vite-plugin-pages'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/blog/',
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/],
